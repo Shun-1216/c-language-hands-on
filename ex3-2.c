b63766f250502a74ad30521c3f9df61a3e96f6fd
Shun-1216 <madjuggler1216@gmail.com>
2024-11-28 03:18:40 +0000

    commit

---
+++ ex3-2.c
@@ -0,0 +1,10 @@
+void main
+{
+    int i;
+    float f;
+    char c;
+
+    i = 10;
+    f = 3.14;
+    c = 'a';
+}
---
+++ ex3-3.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+void main(){
+    int i = 10;
+    float f = 3.14;
+    char c = 'a';
+
+    printf("i = %d f = %f c = %c",i,f,c);
+}
