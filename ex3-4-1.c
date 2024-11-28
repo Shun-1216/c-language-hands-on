045c40dbc3915bae3d5adafee1a48ef398ff3842
Shun-1216 <madjuggler1216@gmail.com>
2024-11-28 06:41:18 +0000

    commit

---
+++ ex3-4-1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main()
+{
+    int x,y,a,b,c,d;
+    x=12,y=2;
+
+    a = x + y;
+    b = x - y;
+    c = x * y;
+    d = x / y;
+
+    printf("a = %d b = %d c = %d d = %d",a,b,c,d);
+}
---
+++ ex3-4-2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+void main()
+{
+    int x = 11,y = 4;
+    float answer = (float)x / (float)y;
+    printf("answer = %f",answer); 
+}
